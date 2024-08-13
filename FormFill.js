d80f85ed0cf19b3fd9d798512c4e73038d1d22b0
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-08-13 19:53:11 +0500

    Update FormFill.js

--- FormFill.js
+++ FormFill.js
@@ -1,8 +1,7 @@
-const optionIndexToSelect = 4 // Set this to the index of the option you want to select (0 for first option, 1 for second, etc.)
-
-document.querySelectorAll('input[type="radio"]').forEach(radio => {
-    const radiosInGroup = document.getElementsByName(radio.name);
-    if (radiosInGroup[optionIndexToSelect]) {
-        radiosInGroup[optionIndexToSelect].checked = true;
-    }
+const optionIndexToSelect = 4;
+document.querySelectorAll('input[type="radio"]').forEach((radio) => {
+  const radiosInGroup = document.getElementsByName(radio.name);
+  if (radiosInGroup[optionIndexToSelect]) {
+    radiosInGroup[optionIndexToSelect].checked = true;
+  }
 });
